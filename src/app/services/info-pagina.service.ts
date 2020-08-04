f25cf271c713d98e94d4818d28e27261f8a08be1
anthony garcia <anthonygarcia55@gmail.com>
2020-08-04 17:20:08 -0400

    Listo Final

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { InfoPagina } from '../interfaces/info-paguina.interface';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,22 +10,38 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
-    // console.log('Servicio de infoPagina listo');
+    
+    this.cargarInfo();
+    this.cargarEquipo();
 
+  }
+
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-
-
-        });
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
 
 
+  private cargarEquipo() {
+
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-9f205.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+      // console.log(resp);
+    });
+
+
+    // this.equipo = resp
   }
 
 }
